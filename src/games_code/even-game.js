ab00d8a8db47a4fe0d5399355e266b614f1b107a
reVoiding <eu.eremin@gmail.com>
2022-04-18 14:55:01 +0700

    Semi fix #4

--- src/games_code/even-game.js
+++ src/games_code/even-game.js
@@ -4,39 +4,35 @@ import readlineSync from 'readline-sync';
 const evenGame = () => {
   // Start
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+
+  // rules
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  // Randomize
+  const randNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
   // Evenness?
   const isEven = (num) => {
     if (num % 2 === 0) {
       return 'yes';
-    }
-    return 'no';
+    } return 'no';
   };
 
-  // Randomize
-  const randNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-  // Hi_and_rules
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  // rounds
   for (let i = 0; i < 3; i += 1) {
-    const num = randNum(1, 99);
-    const correctAnswer = isEven(num);
-    console.log(`Question: ${num}`);
-    const userAnswer = readlineSync.question('Your answer:');
+    const num = randNum(1, 99); // генерим случайно число
+    const correctAnswer = isEven(num); // проверяем первое число на четность
+    console.log(`Question: ${num}`); // задаем вопрос
+    const userAnswer = readlineSync.question('Your answer: '); // получаем ответ игрока
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;( Correct answer was '${correctAnswer}'.`,
-      );
-      console.log(`Let's try again, ${userName}!`);
-      if (i -= 1 < 0) i = 0;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${userName}!`); // отвечаем игроку
+      return;
     }
   }
-  // Bravo!
   console.log(`Congratulations, ${userName}!`);
 };
 
