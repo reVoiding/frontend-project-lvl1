727f7e5e7b1354a20dea64a524fe015d5913ad3e
reVoiding <eu.eremin@gmail.com>
2022-04-18 14:38:38 +0700

    Semi fix #2

--- src/games_code/even-game.js
+++ src/games_code/even-game.js
@@ -1,11 +1,11 @@
 import readlineSync from 'readline-sync';
 
-//Чётность
+  //Чётность
 const evenGame = () => {
-//Start
+  //Start
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-//Evenness?
+  //Evenness?
   const isEven = (num) => {
     if (num % 2 === 0) {
       return 'yes';
@@ -13,15 +13,14 @@ const evenGame = () => {
     return 'no';
   };
 
-//Randomize
-  const randNum = (min, max) =>
-    Math.floor(Math.random() * (max - min + 1)) + min;
+  //Randomize
+  const randNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-//Hi_and_rules
+  //Hi_and_rules
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-//rounds
+  //rounds
   for (let i = 0; i < 3; i += 1) {
     const num = randNum(1, 99);
     const correctAnswer = isEven(num);
